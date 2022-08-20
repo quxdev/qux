d7c86a3f8d3de62a0ce9f91c1e95488f6303e9a7
Vishal Apte <vishalapte@gmail.com>
2022-08-20 22:55:00 +0530

    move company and profile to qux_auth

--- auth/models.py
+++ auth/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.validators import RegexValidator
 from django.db import models
 from django.db.models.signals import post_save
@@ -9,13 +8,16 @@ from qux.models import CoreModel, default_null_blank
 
 
 class Company(CoreModel):
+    SLUG_PREFIX = "company"
+
+    slug = models.CharField(max_length=14, unique=True)
     name = models.CharField(max_length=128)
     address = models.TextField(**default_null_blank)
     domain = models.CharField(max_length=255)
     url = models.URLField(max_length=1024, **default_null_blank)
 
     class Meta:
-        db_table = 'qux_company'
+        db_table = 'qux_auth_company'
         verbose_name = 'Company'
         verbose_name_plural = 'Companies'
 
@@ -25,12 +27,14 @@ class CompanyUser(CoreModel):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'qux_company_users'
-        verbose_name = 'Company:User'
-        verbose_name_plural = 'Company:User'
+        db_table = 'qux_auth_company_users'
+        verbose_name = 'User at Company'
+        verbose_name_plural = 'Users at Companies'
 
 
 class Profile(CoreModel):
+    SLUG_PREFIX = "user"
+
     # https://en.wikipedia.org/wiki/E.164
     regexp = RegexValidator(
         regex=r'^\+?[1-9]\d{4,14}$',
@@ -38,14 +42,19 @@ class Profile(CoreModel):
                 "Up to 15 digits allowed."
     )
 
+    slug = models.CharField(max_length=11, unique=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     phone = models.CharField(max_length=16, validators=[regexp])
     company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, **default_null_blank)
     title = models.CharField(max_length=255, **default_null_blank)
 
     class Meta:
-        db_table = 'qux_user_profile'
+        db_table = 'qux_auth_profile'
         verbose_name = 'User Profile'
+
+    @classmethod
+    def get_user(cls, slug):
+        return cls.objects.get(slug=slug).user
 
     def get_initials(self):
         user = self.user
@@ -77,13 +86,10 @@ class Profile(CoreModel):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created and not hasattr(instance, 'Profile'):
-        p = Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    try:
-        p = instance.profile
-        p.save()
-    except ObjectDoesNotExist:
-        Profile.objects.create(user=instance)
+    p, created = Profile.objects.get_or_create(user=instance)
+    p.save()
