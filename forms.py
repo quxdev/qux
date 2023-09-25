b0dc4d810ba62735a4814a4d21def79e8a9373e4
Vishal Apte <vishalapte@gmail.com>
2023-09-25 08:45:22 +0530

    Bootstrap-enhanced forms.Form and forms.ModelForm

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 
-class QuxForm(forms.ModelForm):
+class QuxForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(QuxForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
@@ -14,3 +14,24 @@ class QuxForm(forms.ModelForm):
 
             if type(visible.field.widget) is forms.widgets.CheckboxInput:
                 visible.field.widget.attrs["class"] = "form-check-input"
+
+            if type(visible.field.widget) is forms.widgets.Select:
+                visible.field.widget.attrs["class"] = "form-select"
+
+
+class QuxModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(QuxModelForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = "form-control"
+
+            # Identify file uploads and adjusts
+            # getbootstrap.com/docs/4.6/components/input-group/#custom-file-input
+            if type(visible.field.widget) is forms.widgets.ClearableFileInput:
+                visible.field.widget.attrs["class"] += " custom-file-input"
+
+            if type(visible.field.widget) is forms.widgets.CheckboxInput:
+                visible.field.widget.attrs["class"] = "form-check-input"
+
+            if type(visible.field.widget) is forms.widgets.Select:
+                visible.field.widget.attrs["class"] = "form-select"
